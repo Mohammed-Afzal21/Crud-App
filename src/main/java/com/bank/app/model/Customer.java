54608272ef6f3dd55fe312e50d02faaa20178875
Mohammed Afzal <mohammedafzalk07@gmail.com>
2026-03-16 20:13:08 +0530

    add full project

---
+++ src/main/java/com/bank/app/model/Customer.java
@@ -0,0 +1,68 @@
+package com.bank.app.model;
+
+import jakarta.persistence.*;
+
+
+@Entity
+public class Customer {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+    private String phoneNumber;
+
+    public Customer() {
+    }
+
+    public Customer(String phoneNumber, String email, String lastName, String firstName, Long id) {
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
